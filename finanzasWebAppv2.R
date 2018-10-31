a499afff4c1aee463b295a63c61bd8a3b373968e
flynngodslayer <victor_avila_felipe@hotmail.com>
2018-10-30 18:31:42 -0600

    Added scrollbar, and button to save the data into local file

--- finanzasWebAppv2.R
+++ finanzasWebAppv2.R
@@ -3,14 +3,20 @@ library(shinydashboard)
 library(DT)
 library(tidyverse)
 library(dplyr)
+setwd("/home/curso/finanzasShinyR")
 
 ui <- dashboardPage(
         dashboardHeader(),
         dashboardSidebar(),
         dashboardBody(
-        fileInput("file", "Adjunta Archivo", width = "400px"),
-        mainPanel(
-          tabPanel("Tabla de datos", DTOutput("file")))
+          fileInput("file", "Adjunta Archivo", width = "400px"),
+          mainPanel(
+            tabPanel("Tabla de datos", DTOutput("file")),
+            conditionalPanel(
+              condition = "output.file",
+              actionButton("escribir", "Guardar CSV")
+            )
+          )
         )
 )
 
@@ -18,6 +24,7 @@ server <- function(input, output) {
 
     output$file <- renderDT({
       req(input$file)
+      
       archivo <-read.csv(input$file$datapath, header = TRUE, skip = 1,
                                     colClasses = c("character", "character", "character", "character",
                                                    "character", "character"))
@@ -26,8 +33,15 @@ server <- function(input, output) {
       archivo$credito <- as.numeric(gsub(",", "", archivo$credito))
       archivo$debito[!is.na(archivo$credito)] <- -archivo$credito[!is.na(archivo$credito)]
       archivo <- select(archivo, -credito, -saldo, -moneda)
-      datatable(archivo, selection = "none", editable = TRUE)
+      archivo
+      datatable(archivo, selection = "none", editable = TRUE, options = list(scrollY = '300px', scrollX = TRUE, paging = FALSE, searching = FALSE))
     })
+    
+    observeEvent(
+      input$escribir, {
+        write.csv(archivo, file = "memoria.csv")
+      }
+    ) 
   }
 
 shinyApp(ui, server)
