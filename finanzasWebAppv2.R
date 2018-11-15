fa641e81eba7d675e8cfb045196792be1076c31d
Ricardo Ruiz <haloplatinum@hotmail.es>
2018-11-14 18:01:46 -0600

    The categories and the dropdown of the dates were added

--- finanzasWebAppv2.R
+++ finanzasWebAppv2.R
@@ -5,46 +5,52 @@ library(tidyverse)
 library(dplyr)
 library(DBI)
 library(RSQLite)
+library(rhandsontable)
 
 
 ui <- dashboardPage(
-        dashboardHeader(),
-        dashboardSidebar(),
-        dashboardBody(
-          fileInput("file", "Adjunta Archivo", width = "300px"),
-          mainPanel(
-            tabPanel("Tabla de datos", DTOutput("file")),
-            conditionalPanel(
-              condition = "output.file",
-              actionButton("save", "Guardar CSV"),
-              selectInput("categoria", label = "Categoria:"  ,choice = c("Comida", "transporte"))
-            )
-          )
-        )
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fileInput("ar", "Adjunta Archivo", width = "300px"),
+    mainPanel(
+      rHandsontableOutput("file")
+    )
+  )
 )
 
+
 server <- function(input, output) { 
- 
-    output$file <- renderDT({
-      req(input$file)
-      
-      archivo <-read.csv(input$file$datapath, header = TRUE, skip = 1,
-                                    colClasses = c("character", "character", "character", "character",
-                                                   "character", "character"))
-      colnames(archivo) <- c("fecha", "descripcion", "debito", "credito", "saldo", "moneda")
-      archivo$debito <- as.numeric(gsub(",", "", archivo$debito ))
-      archivo$credito <- as.numeric(gsub(",", "", archivo$credito))
-      archivo$debito[!is.na(archivo$credito)] <- -archivo$credito[!is.na(archivo$credito)]
-      categoria <- vector(mode = 'character', length = 41)
-      categoria <- input$categoria
-      archivo <- data.frame(archivo, categoria)
-      ar <- select(archivo, -credito, -saldo, -moneda)
-      datatable(ar, selection ="none", editable = TRUE,
-                options = list(scrollY = '400px', scrollX = TRUE, paging = FALSE, 
-                               searching = TRUE))
-     
-    })
-     
-  }
+  
+  output$file <- renderRHandsontable({
+    req(input$ar)
+    archivo <-read.csv(input$ar$datapath, header = TRUE, skip = 1,
+                       colClasses = c("character", "character", "character", "character",
+                                      "character", "character"))
+    colnames(archivo) <- c("fecha", "descripcion", "debito", "credito", "saldo", "moneda")
+    archivo$debito <- as.numeric(gsub(",", "", archivo$debito ))
+    archivo$credito <- as.numeric(gsub(",", "", archivo$credito))
+    archivo$debito[!is.na(archivo$credito)] <- -archivo$credito[!is.na(archivo$credito)]
+    categoria <- vector(mode = 'character', length = 41)
+    fecha <- seq(from = Sys.Date(), by = "days", length.out = 10)
+    #categoria <- factor(LETTERS[41:1],levels =LETTERS[41:1])
+    archivo <- data.frame(archivo, categoria)
+    ar <- select(archivo, -credito, -saldo, -moneda)
+    rhandsontable(ar, width = 600, height = 300, selectCallback = TRUE)%>%
+      hot_col(col = "categoria", type = 'dropdown', source = c("Comida", "Entretenimiento", 
+                                                               "Oficina", "Infraestructura", 
+                                                               "Transporte", "Compras", "Ropa", 
+                                                               "Teléfono", "Salud", "Deporte", 
+                                                               "Electrónica", "Cuidado personal",
+                                                               "Mascota", "Viajes", "Educación", 
+                                                               "Impuestos", "Automóvil", 
+                                                               "Estados de cuenta" ))%>%
+      hot_col(col = "fecha", type = 'dropdown')
+    #datatable(ar, selection =list(target = "cell"), editable = TRUE,
+    #options = list(scrollY = '400px', scrollX = TRUE, paging = FALSE, 
+    #searching = TRUE))
+  })
+  
+}
 
 shinyApp(ui, server)
