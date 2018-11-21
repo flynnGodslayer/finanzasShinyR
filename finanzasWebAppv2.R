98679a63d9fd6c5da511a7138986e05bbae2ee2b
Ricardo Ruiz <haloplatinum@hotmail.es>
2018-11-21 17:26:35 -0600

    reupload changes were made to the structure and a menu bar was placed

--- finanzasWebAppv2.R
+++ finanzasWebAppv2.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 list.of.packages <- c("shiny", "shinydashboard","DT","tidyverse","dplyr","DBI","RSQLite","rhandsontable","sweetalertR","formattable")
 
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
@@ -100,43 +99,6 @@ body <- dashboardBody(
     ),
     #MenuItem, dashboard
     tabItem(tabName = "dashboard")
-=======
-library(shiny)
-library(shinydashboard)
-library(DT)
-library(tidyverse)
-library(dplyr)
-library(DBI)
-library(RSQLite)
-library(rhandsontable)
-library(sweetalertR)
-
-
-ui <- dashboardPage(
-  dashboardHeader(),
-  dashboardSidebar(),
-  dashboardBody(
-    fileInput("ar", "Adjunta Archivo", width = "300px"),
-    mainPanel(
-      rHandsontableOutput("file"),
-      conditionalPanel(
-        condition = "output.file",
-        actionButton("firstConf", "Guardar Registros"),
-        sweetalert(selector = "#firstConf", 
-                   text = "¿Seguro que desea guardar los cambios?", 
-                   title = "¡Atención!", 
-                   showCancelButton = TRUE,
-                   cancelButtonText = 'Atras',
-                   confirmButtonColor = '#39FF14',
-                   confirmButtonText = 'Guardar cambios',
-                   closeOnConfirm = FALSE,
-                   evalFunction = 'function(){
-                                    swal("Cambios guardados exitosamente", "Tus gastos han sido guardados exitosamente", "success")
-                                  }
-        ')
-      )
-    )
->>>>>>> bdddf1f36a4fb06fff59306ab45011374c25e060
   )
 )
 
@@ -149,7 +111,6 @@ ui <- dashboardPage(header = header,
 #Server#
 ########
 
-<<<<<<< HEAD
 server <- function(input, output) {
   #Funcion render conectada con rHandsontableOutput
   output$Tabla <- renderRHandsontable({
@@ -190,39 +151,6 @@ server <- function(input, output) {
                            "Impuestos", "Automóvil","Estados de cuenta")
         )
     }
-=======
-server <- function(input, output) { 
-  
-  output$file <- renderRHandsontable({
-    req(input$ar)
-    archivo <-read.csv(input$ar$datapath, header = TRUE, skip = 1,
-                       colClasses = c("character", "character", "character", "character",
-                                      "character", "character"))
-    colnames(archivo) <- c("fecha", "descripcion", "debito", "credito", "saldo", "moneda")
-    archivo$debito <- as.numeric(gsub(",", "", archivo$debito ))
-    archivo$credito <- as.numeric(gsub(",", "", archivo$credito))
-    archivo$debito[!is.na(archivo$credito)] <- -archivo$credito[!is.na(archivo$credito)]
-    categoria <- vector(mode = 'character', length = 41)
-    #categoria <- factor(LETTERS[41:1],levels =LETTERS[41:1])
-    archivo <- data.frame(archivo, categoria)
-    ar <- select(archivo, -credito, -saldo, -moneda)
-    rhandsontable(ar, width = 700, stretchH = "all",height = 300, selectCallback = TRUE)%>%
-      hot_col(col = "categoria", type = 'dropdown', source = c("Comida", "Entretenimiento", 
-                                                               "Oficina", "Infraestructura", 
-                                                               "Transporte", "Compras", "Ropa", 
-                                                               "Teléfono", "Salud", "Deporte", 
-                                                               "Electrónica", "Cuidado personal",
-                                                               "Mascota", "Viajes", "Educación", 
-                                                               "Impuestos", "Automóvil", 
-                                                               "Estados de cuenta" ))%>%
-      hot_col(col = "fecha", type = 'date', source = Sys.Date())%>%
-      hot_context_menu(allowRowEdit = TRUE, allowColEdit = TRUE)%>%
-      hot_cols(colWidths = 160) %>%
-      hot_cols(fixedColumnsLeft = 1) 
-    #datatable(ar, selection =list(target = "cell"), editable = TRUE,
-    #options = list(scrollY = '400px', scrollX = TRUE, paging = FALSE, 
-    #searching = TRUE))
->>>>>>> bdddf1f36a4fb06fff59306ab45011374c25e060
   })
   #Guarda los cambios en la Tabla, de la ruta que tomamos el arhivo.
   observeEvent(input$Btn_Guardar,
